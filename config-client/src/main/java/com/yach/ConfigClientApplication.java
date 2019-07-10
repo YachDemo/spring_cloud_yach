e12995ddf578f3e9f5d09966d1b1a2d7c9d85a63
Yach <2291900019@qq.com>
2019-07-10 18:33:38 +0800

    修改bug，添加远程client服务

---
+++ config-client/src/main/java/com/yach/ConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.yach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+        System.out.println("=================================ConfigClientApplication Start========================");
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+        c.setIgnoreUnresolvablePlaceholders(true);
+        return c;
+    }
+}
---
+++ config-client/src/main/java/com/yach/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.yach.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Test
+ * 读取配置服务文件
+ * Created by Yach on 2019/7/10
+ * @version v4.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/hi")
+    public String hi(){
+        return foo;
+    }
+}
