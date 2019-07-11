3e2145f100453e34ace7ebec47e5e3062a968dc2
Yach <2291900019@qq.com>
2019-07-11 18:08:06 +0800

    添加文件

---
+++ service-hi/src/main/java/com/yach/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.yach.service.impl;
+
+import com.yach.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * user实现
+ * Created by Yach on 2019/7/11
+ * @version v1.0
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
