04032c53a7936281a4cfb7af76fe5ddbad3cd075
Yach <2291900019@qq.com>
2019-08-15 09:35:21 +0800

    添加mybatis自动生成xml

---
+++ mq-consumers/src/main/java/com/mq/component/MsgReceiver.java
@@ -0,0 +1,25 @@
+package com.mq.component;
+
+import com.mq.config.RabbitConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消费消息
+ * Created by Yach on 2019/8/14
+ * @version v4.0
+ */
+@Component
+@RabbitListener(queues = RabbitConfig.QUEUE_A)
+public class MsgReceiver {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RabbitHandler
+    public void process(String content){
+        logger.info("接收处理队列A的消息："+content);
+    }
+
+}
